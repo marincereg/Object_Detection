1e3551ce1df6c85acc7d2b3e9fc5a8914f0f52aa
marincereg <marin.cereg@gmail.com>
2022-11-10 16:37:38 -0500

    Update - HMI added and image collection modified
    Added HMI and modified HMI so there is no more console writes.

--- HMI.py
+++ HMI.py
@@ -17,8 +17,25 @@ root.config(bg='#78909c')
 from tkinter import *
 from tkinter import ttk
 
+import CollectImages
+import LogCreator
+
+def Collect ():
+    LogCreator.Log_MSG("[HMI.py ] Collect Images call ")
+    In1 = InputDirectoryName.get()
+    in2 = NumberOfImages.get()
+    LogCreator.Log_MSG("[HMI.py ] Collect Images parameters : InputDirectoryName =  "  +str(In1) + " and Number of images = " +str(in2))
+    CollectImages.CollectImage(In1, int(in2))
+    
+def LabelApp ():
+    LogCreator.Log_MSG("[HMI.py ] Labeling App is Starting ")
+    CollectImages.StartLabelingApp()
+
+LogCreator.SW_InRun()
+LogCreator.Log_MSG("[HMI.py ] Started ")
 # Create an instance of tkinter frame
 win = Tk()
+LogCreator.Log_MSG("[HMI.py ] Instance of Gui created ")
 
 # Set the size of the tkinter window
 win.geometry("500x400")
@@ -44,7 +61,34 @@ nb.add(f1, text= 'Collect Images')
 nb.add(f2, text= "Train Model")
 nb.add(f3, text= "Phun Tab")
 
+# Objects in Collect Images
+
+Label(f1, text="Input name of direcotry :", bg="#78909c", fg="white").place(x=10, y=20)
+InputDirectoryName = Entry( f1 , width= 30)
+InputDirectoryName.configure(bd=5)
+InputDirectoryName.place(x=180, y=20)
+
+Label(f1, text="Input number of images :", bg="#78909c", fg="white").place(x=10, y=80)
+NumberOfImages = Entry( f1, width= 10)
+NumberOfImages.configure(bd=5)
+NumberOfImages.place(x=180, y=80)
+
+Label(f1, text="Create Images", bg="#78909c", fg="white").place(x=100, y=140)
+Button(f1, text="Accept", command= lambda : Collect()).place(x=210, y=137)
+
+Label(f1, text="Start App for Labeling", bg="#78909c", fg="white").place(x=10, y=230)
+Button(f1, text="Start App", command= lambda : LabelApp()).place(x=160, y=229)
+
+# Label For Train Model
+label = Label(f2, text='This is a label')
+label.pack(ipadx=10, ipady=10)
+
+# Label For Phun Tab
+label = Label(f3, text='This is a label')
+label.pack(ipadx=10, ipady=10)
+
 nb.pack(expand= True, fill=BOTH, padx= 5, pady=5)
+
 win.mainloop()
 
 
--- HMI_TEST.py
+++ HMI_TEST.py
@@ -1,63 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  9 16:14:34 2022
-
-@author: marin
-"""
-
-
-
-"""
-GUI
-"""
 import tkinter as tk
-from tkinter import ttk
 
+gui = tk.Tk()
 
+gui.geometry("300x300")
 
-class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.geometry("400x400")
-        self.config(bg='#78909c')
-        self.title('Object Detection')
+def getEntry():
+    res = MyEntry.get()
+    print(res)
 
-        # initialize data
-        self.languages = ('Python', 'JavaScript', 'Java',
-                        'Swift', 'GoLang', 'C#', 'C++', 'Scala')
+MyEntry = tk.Entry(gui,width = 40 )
+MyEntry.pack()
 
-        # set up variable
-        self.option_var = tk.StringVar(self)
+btn = tk.Button(gui, text="Try" , command = lambda : getEntry())
+btn.pack()
 
-        # create widget
-        self.create_wigets()
-
-    def create_wigets(self):
-        # padding for widgets using the grid layout
-        paddings = {'padx': 5, 'pady': 5}
-
-        # label
-        label = ttk.Label(self,  text='Select your most favorite language:')
-        label.grid(column=0, row=0, sticky=tk.W, **paddings)
-
-        # option menu
-        option_menu = ttk.OptionMenu(
-            self,
-            self.option_var,
-            self.languages[0],
-            *self.languages,
-            command=self.option_changed)
-
-        option_menu.grid(column=1, row=0, sticky=tk.W, **paddings)
-
-        # output label
-        self.output_label = ttk.Label(self, foreground='red')
-        self.output_label.grid(column=0, row=1, sticky=tk.W, **paddings)
-
-    def option_changed(self, *args):
-        self.output_label['text'] = f'You selected: {self.option_var.get()}'
-
-
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+gui.mainloop()
